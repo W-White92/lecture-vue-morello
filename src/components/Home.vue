824971b3906efe77d87e722ea44d3f303b0d88a4
SangHyeok <rktke394@gmail.com>
2022-07-09 23:55:17 +0900

    axios를 래핑해서 사용하는 방법: api-service 구현

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import {board} from '../api'
 
 export default {
     data() {
@@ -44,14 +44,18 @@ export default {
     methods: {
     fetchData() {
         this.loading = true
-        axios.get('http://localhost:3000/boards')
-            .then(res => {
-                this.apiRes = res.data
+        board.fetch()
+            .then(data => {
+                this.boards = data
             })
-            .catch(res => {
-                // this.error = res.response.data
-                this.$router.replace('login')
-            })
+        // axios.get('http://localhost:3000/boards')
+        //     .then(res => {
+        //         this.apiRes = res.data
+        //     })
+            // .catch(res => {
+            //     // this.error = res.response.data
+            //     this.$router.replace('login')
+            // })
             .finally(_=> {
                 this.loading = false
             })
