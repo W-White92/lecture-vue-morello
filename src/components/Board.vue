c034c6da437aa49845ca69c9b8b84aaa5217aa6d
SangHyeok <rktke394@gmail.com>
2022-07-09 18:10:23 +0900

    보드 컴포넌트 fetchData

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,8 +2,13 @@
   <div>
     Board
     <div>bid: {{bid}}</div>
-    <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
-    <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+    <!-- /데이터 로딩 완료시 출력하도록 -->
+    <div v-if="loading">loading board...</div>
+    <div v-else>
+      <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+      <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+    </div>
+    <!-- /데이터 로딩 완료시 출력하도록 -->
     <hr />
     <router-view></router-view>
   </div>
@@ -13,11 +18,21 @@
 export default {
     data() {
         return {
-            bid: 0
+            bid: 0,
+            loading: false
         }
     },
     created() {
-        this.bid = this.$route.params.bid
+        this.fetchData()
+    },
+    methods: {
+      fetchData() {
+        this.loading = true
+        setTimeout(() => {
+          this.bid = this.$route.params.bid
+          this.loading = false
+        }, 500)
+      }// 백엔드 호출하는 함수
     }
 }
 </script>
