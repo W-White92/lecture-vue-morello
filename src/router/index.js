aa0a225cfee80de186a027f5c64b02c785589a4c
SangHyeok <rktke394@gmail.com>
2022-07-09 18:01:00 +0900

    /b/:bid/c/:cid 중첩 라우팅 추가

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,11 @@ import VueRouter from 'vue-router';
 // import App from '../App.vue'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
-import NotFound from '../components/NotFound.vue'
 import Board from '../components/Board.vue'
+import Card from '../components/Card.vue'
+import NotFound from '../components/NotFound.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter) // 미들웨어
 
 // const Login = { template: '<div>Login Page</div>' }
 // const NotFound = { template: '<div>Page not found</div>' }
@@ -16,7 +17,9 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/login', component: Login },
-    { path: '/b/:bid', component: Board },
+    { path: '/b/:bid', component: Board, children: [
+      { path: 'c/:cid', component: Card },
+    ] }, 
     { path: '/*', component: NotFound }
   ] 
 })
